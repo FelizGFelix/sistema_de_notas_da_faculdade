5b1bf7f7bae6a91890b837115bbd4e5fae689e25
Felipe Felix <lipegfelix@gmail.com>
2026-04-08 19:17:50 -0300

    sistema em nota parte 2

--- notas.py
+++ notas.py
@@ -1,5 +1,11 @@
 # notas do 1 semestre
 
+import os
+
+def clear_screen():
+    command = 'cls' if os.name == 'nt' else 'clear'
+    os.system(command)
+
 def valor1():
     resposta = float(input("Insira a sua nota: "))
     notas.append(resposta)
@@ -23,6 +29,10 @@ def valor4():
 def main():
     print("1 Eng de prompt de IA\n2 Eletroeletrônica\n3 Modelagem\n4 Programação\n5 acessar as notas")
 
+def main4():
+    print("1 Eng de prompt de IA\n2 Eletroeletrônica\n3 Modelagem\n4 Programação")
+
+
 notas = []
 notas2 = []
 notas3 = []
@@ -31,73 +41,85 @@ notas4 = []
 def main2():
 
     try:
-        valor = int(input("Digite um dos valores para visitar: "))
+        valor = int(input("Digite um dos valores para visitar: \n-> "))
 
         if valor == 1:
             valor1()
             print(notas)
             input("Digite qualquer tecla para voltar: ")
-            main()
-            main2()
+            clear_screen()
+            main3()
 
         elif valor == 2:
             valor2()
             print(notas2)
             input("Digite qualquer tecla para voltar: ")
+            clear_screen()
             main3()
 
         elif valor == 3:
             valor3()
             print(notas3)
             input("Digite qualquer tecla para voltar: ")
+            clear_screen()
             main3()
 
         elif valor == 4:
             valor4()
             print(notas4)
             input("Digite qualquer tecla para voltar: ")
+            clear_screen()
             main3()
 
         elif valor == 5:
-            main()
+            clear_screen()
+            main4()
             resposta = int(input("Qual nota deseja acessar? "))
             if resposta == 1:
                 print(notas)
                 input("Digite qualquer tecla para voltar: ")
+                clear_screen()
                 main3()
 
             elif resposta == 2:
                 print(notas2)
                 input("Digite qualquer tecla para voltar: ")
+                clear_screen()
                 main3()
 
             elif resposta == 3:
                 print(notas3)
                 input("Digite qualquer tecla para voltar: ")
+                clear_screen()
                 main3()
 
             elif resposta == 4:
                 print(notas4)
                 input("Digite qualquer tecla para voltar: ")
+                clear_screen()
                 main3()
         
             elif resposta == 5:
                 print("As notas já estão sendo acessadas")
+                clear_screen()
                 main3()
 
             else:
                 print("Insira um valor válido")
+                clear_screen()
                 main3()
 
 
         else:
             print("Insira um valor válido")
             input("Digite qualquer tecla para voltar: ")
+            clear_screen()
             main3()
 
     except:
         print("Insira um valor válido")
         input("Digite qualquer tecla para voltar: ")
+        clear_screen()
         main3()
 
 def main3():
@@ -106,5 +128,3 @@ def main3():
 
 if __name__ == "__main__":
     main3()
-
-    
